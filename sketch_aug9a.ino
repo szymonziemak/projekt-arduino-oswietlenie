78d56fe487907d4b47c8a7d10aea26be39c9b852
Szymon Ziemak <sy.ziemak@gmail.com>
2025-08-20 14:06:45 +0200

    Update sketch_aug9a.ino

--- sketch_aug9a.ino
+++ sketch_aug9a.ino
@@ -2,11 +2,11 @@
 #include <SinricPro.h>
 #include <SinricProSwitch.h>
 
-#define WIFI_SSID         "TP-Link_54CF"
-#define WIFI_PASS         "75216627"
-#define APP_KEY           "3e1fe396-32cf-4584-a4e1-27beeedf0cc7"
-#define APP_SECRET        "24065007-9cd1-4b5e-8480-519dddc6df3b-8d206304-af48-4131-8319-1ceb3bbe2348"
-#define SWITCH_ID         "68975b82678c5bc9ab28118c"
+#define WIFI_SSID         ""
+#define WIFI_PASS         ""
+#define APP_KEY           ""
+#define APP_SECRET        ""
+#define SWITCH_ID         ""
 
 #define RELAY_PIN         14 // Przykładowy pin, dostosuj do swojego połączenia
 
